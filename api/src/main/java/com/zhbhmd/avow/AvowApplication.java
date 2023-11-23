7956ac5e6e976fbe2e0d3d706cfdc01e9b599de2
Zohaib Ahmed <zohebahmed111@hotmail.com>
2023-11-23 08:55:33 +0100

    api setup

---
+++ api/src/main/java/com/zhbhmd/avow/AvowApplication.java
@@ -0,0 +1,13 @@
+package com.zhbhmd.avow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvowApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/zhbhmd/avow/AvowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhbhmd.avow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
