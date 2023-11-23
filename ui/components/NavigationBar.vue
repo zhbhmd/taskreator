696ad2bbd506e737c49a9c9261460577f51d3fe0
Zohaib Ahmed <zohebahmed111@hotmail.com>
2023-11-23 12:50:54 +0100

    navbar and layout

---
+++ ui/components/NavigationBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="bg-gray-800 p-4 text-white">
+    <div class="container mx-auto">
+      <!-- Navbar content goes here -->
+      <div class="flex justify-between items-center">
+        <div class="text-2xl font-semibold font-">
+          Taskreator
+        </div>
+        <div class="flex justify-between items-center">
+          <NuxtLink class="mr-8" to="/">
+            Listing
+          </NuxtLink>
+          <NuxtLink class="mr-8" to="/create">
+            Create
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavigationBar'
+}
+</script>
---
+++ ui/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <NavigationBar />
+    <main>
+      <Nuxt />
+    </main>
+  </div>
+</template>
